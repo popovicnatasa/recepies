a0ac1235d14cc2df3f58828a741c50e1437f25f2
popovicnatasa <natasha.popovic94@gmail.com>
2022-07-29 15:11:23 +0200

    Povezan mealDB

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import './App.css';
 import * as React from 'react';
-//import { useState, useEffect } from 'react';
-//import Button from '@mui/material/Button';
-//import CssBaseline from '@mui/material/CssBaseline';
-//import TextField from '@mui/material/TextField';
-//import Box from '@mui/material/Box';
-//import Typography from '@mui/material/Typography';
-//import Container from '@mui/material/Container';
-//import {  createTheme } from '@mui/material/styles';
-//import { Grid, Avatar, FormControlLabel, Checkbox } from '@mui/material';
-//import { Link } from '@mui/material';
-
-//import Registration from './components/Registration';
 import Login from './components/Login';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer'
 import { Route, Routes } from 'react-router-dom';
 import Ingredients from './components/Ingredients';
-
-//const theme = createTheme();
-
-//function handleSubmit(e) {
-//  console.log('submit');
-//}
+import Ingredient from './components/Ingredient';
 
 function App() {
   return (
@@ -34,6 +17,7 @@ function App() {
         <Route exact path="/aboutus" element={<HomePage></HomePage>}></Route>
         <Route exact path="/recipes" element={<HomePage></HomePage>}></Route>
         <Route exact path="/ingredients" element={<Ingredients></Ingredients>}></Route>
+        
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
       <Footer></Footer>
--- src/components/IngredientCard.js
+++ src/components/IngredientCard.js
@@ -10,26 +10,26 @@ function IngredientCard(props) {
 
     return (
         <Grid item >
-           <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={props.image}
-                    alt={props.name}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {props.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {props.description}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={props.image}
+                        alt={props.name}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {props.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {props.description}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
         </Grid>
-       
+
     );
 }
 
