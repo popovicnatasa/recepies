c3b088226205956b28eedd395312dd4f6e4f73c9
popovicnatasa <natasha.popovic94@gmail.com>
2022-08-01 10:14:32 +0200

    Dialog ingredient

--- src/components/IngredientCard.js
+++ src/components/IngredientCard.js
@@ -5,31 +5,46 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { Grid } from '@mui/material';
+import ScrollDialog from './ScrollDialog';
 
 function IngredientCard(props) {
 
-    return (
-        <Grid item >
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={props.image}
-                        alt={props.name}
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {props.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {props.description}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </Grid>
+    const [open, setOpen] = React.useState(false);
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+    return (
+        
+            <Grid item >
+                <ScrollDialog open={open} handleClickOpen={handleClickOpen} handleClose={handleClose} name={props.name} description={props.long}></ScrollDialog>
+
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea onClick={handleClickOpen}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={props.image}
+                            alt={props.name}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {props.name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {props.description}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Grid>
+        
     );
 }
 
